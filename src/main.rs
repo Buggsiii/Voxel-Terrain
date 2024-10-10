1f8eceef6cc4b8f003e157387c43d0adf5537d2d
Bugge <fbj070106@gmail.com>
2024-10-10 19:45:05 +0200

    Unoptimized Chunk

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,105 @@
-use bevy::core_pipeline::core_2d::graph::input;
-use bevy::prelude::*;
-use bevy::render::{
-    mesh::{Indices, VertexAttributeValues},
-    render_asset::RenderAssetUsages,
-    render_resource::PrimitiveTopology,
+use bevy::{
+    color::palettes::css::WHITE,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    pbr::wireframe::{WireframeConfig, WireframePlugin},
+    prelude::*,
 };
+
+mod unoptimized_terrain;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_plugins((
+            DefaultPlugins,
+            FrameTimeDiagnosticsPlugin,
+            LogDiagnosticsPlugin::default(),
+            WireframePlugin,
+        ))
+        .insert_resource(WireframeConfig {
+            global: true,
+            default_color: WHITE.into(),
+        })
+        .add_systems(Startup, (setup, setup_camera))
+        // Unoptimized terrain
+        .add_systems(Startup, unoptimized_terrain::setup)
         .add_systems(Update, input_handler)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(15.0, 5.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-
+fn setup(mut commands: Commands) {
     commands.spawn(DirectionalLightBundle {
         transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4)),
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
         ..default()
     });
+}
 
-    commands.spawn(
-        PbrBundle {
-            mesh: meshes.add(Plane3d::default().mesh().size(20., 20.)),
-            material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-15.0, 5.0, -15.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
-        }
-    );
-
-    // let cube_mesh_handle: Handle<Mesh> = create_cube_mesh();
-    // commands.spawn(PbrBundle {
-    //     mesh: cube_mesh_handle,
-    //     ..default()
-    // });
+        },
+        // FogSettings {
+        //     color: Color::srgb(0.168627451, 0.17254902, 0.184313725),
+        //     falloff: FogFalloff::Linear {
+        //         start: 5.0,
+        //         end: 20.0,
+        //     },
+        //     ..default()
+        // },
+    ));
 }
 
 fn input_handler(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mesh_query: Query<&Handle<Mesh>, With<Camera>>,
-    mut meshes: ResMut<Assets<Mesh>>,
     mut query: Query<&mut Transform, With<Camera>>,
-    time: Res<Time>
+    time: Res<Time>,
 ) {
+    let speed = 10.0;
+    let rot_speed = 1.;
+    let delta_time = time.delta_seconds();
+
     for mut transform in &mut query {
         if keyboard_input.pressed(KeyCode::KeyW) {
             let dir = transform.local_z();
-            transform.translation -= dir * 0.1;
+            transform.translation -= dir * speed * delta_time;
         }
         if keyboard_input.pressed(KeyCode::KeyS) {
             let dir = transform.local_z();
-            transform.translation += dir * 0.1;
+            transform.translation += dir * speed * delta_time;
         }
+
         if keyboard_input.pressed(KeyCode::KeyA) {
             let dir = transform.local_x();
-            transform.translation -= dir * 0.1;
+            transform.translation -= dir * speed * delta_time;
         }
         if keyboard_input.pressed(KeyCode::KeyD) {
             let dir = transform.local_x();
-            transform.translation += dir * 0.1;
+            transform.translation += dir * speed * delta_time;
+        }
+
+        if keyboard_input.pressed(KeyCode::ShiftLeft) {
+            transform.translation.y += speed * delta_time;
+        }
+        if keyboard_input.pressed(KeyCode::ControlLeft) {
+            transform.translation.y -= speed * delta_time;
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            transform.rotate_local_x(rot_speed * delta_time);
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            transform.rotate_local_x(-rot_speed * delta_time);
+        }
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            transform.rotate_y(rot_speed * delta_time);
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            transform.rotate_y(-rot_speed * delta_time);
         }
     }
 }
-
-// fn create_cube_mesh() -> Mesh {
-
-// }
